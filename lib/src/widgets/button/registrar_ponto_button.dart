a1f15b9e7f3e3a02f23ff91658e371e08ce274e5
henriquenicolli <henrique.nicolli@gmail.com>
2024-02-07 17:09:08 -0300

    refactor registro de ponto

--- lib/src/widgets/button/registrar_ponto_button.dart
+++ lib/src/widgets/button/registrar_ponto_button.dart
@@ -6,6 +6,74 @@ import 'dart:convert';
 class RegistrarPontoButton extends StatelessWidget {
   const RegistrarPontoButton({super.key});
 
+  Future<http.Response> registraPonto(BuildContext context) async {
+    final String dataHoraRegistroPonto = DateTime.now().toIso8601String();
+
+    http.Response response = await http.post(
+      Uri.parse('http://localhost:10000/v1/bateponto/registrar'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'dataHoraRegistroPonto': dataHoraRegistroPonto,
+      }),
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 202) {
+      // Exibir Snackbar de sucesso
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Ponto registrado com sucesso'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else {
+      // Exibir Snackbar de erro
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erro durante registro de ponto'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+
+    return response;
+  }
+
+  Future<void> _registrarPontoDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Registrar ponto'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Deseja confirmar o registro de ponto?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => {
+                Navigator.pop(context, 'Cancel'),
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () => {
+                registraPonto(context),
+                Navigator.pop(context, 'OK'),
+              },
+              child: const Text('Registrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -13,8 +81,7 @@ class RegistrarPontoButton extends StatelessWidget {
       height: 70,
       child: ElevatedButton(
         onPressed: () {
-          // Ação a ser executada ao pressionar o botão
-          registraPonto();
+          _registrarPontoDialog(context);
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: AppLayoutDefaults.secondaryColor,
@@ -29,20 +96,6 @@ class RegistrarPontoButton extends StatelessWidget {
           style: TextStyle(fontSize: 20),
         ),
       ),
-    );
-  }
-
-  Future<http.Response> registraPonto() {
-    final String dataHoraRegistroPonto = DateTime.now().toIso8601String();
-
-    return http.post(
-      Uri.parse('http://localhost:10000/v1/bateponto/registrar'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'dataHoraRegistroPonto': dataHoraRegistroPonto,
-      }),
     );
   }
 }
