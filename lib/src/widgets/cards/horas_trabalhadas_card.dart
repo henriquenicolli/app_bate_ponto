5b245551d7a27857c4c487e772ce0ae70f5309ad
henriquenicolli <henrique.nicolli@gmail.com>
2024-03-19 19:25:20 -0300

    code refactor

--- lib/src/widgets/cards/horas_trabalhadas_card.dart
+++ lib/src/widgets/cards/horas_trabalhadas_card.dart
@@ -4,9 +4,10 @@ import 'package:flutter_app_bate_ponto/src/model/registro_ponto_atual_snapshot.d
 import 'package:flutter_app_bate_ponto/src/widgets/button/toggle_button.dart';
 
 class HorasTrabalhadasCard extends StatefulWidget {
-  RegistroPontoAtualSnapshot registroPontoSnapshot;
+  final RegistroPontoAtualSnapshot registroPontoSnapshot;
 
-  HorasTrabalhadasCard({super.key, required this.registroPontoSnapshot});
+  const HorasTrabalhadasCard({Key? key, required this.registroPontoSnapshot})
+      : super(key: key);
 
   @override
   State<HorasTrabalhadasCard> createState() => _HorasTrabalhadasCardState();
@@ -16,12 +17,41 @@ class _HorasTrabalhadasCardState extends State<HorasTrabalhadasCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(18.0),
+      margin: const EdgeInsets.all(18.0),
       child: Column(
         children: [
           CustomToggleButtons(),
           HorasTrabalhadas(),
-          EntradasSaidasText(text1: 'ENTRADAS', text2: 'SAIDAS'),
+          const Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Spacer(flex: 5),
+                  Text(
+                    'ENTRADAS',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      fontFamily: AppLayoutDefaults.fontFamily,
+                    ),
+                  ),
+                  Spacer(flex: 5),
+                  Text(
+                    'SAIDAS',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      fontFamily: AppLayoutDefaults.fontFamily,
+                    ),
+                  ),
+                  Spacer(flex: 5),
+                ],
+              ),
+            ),
+          ),
           ListView.builder(
             shrinkWrap: true,
             itemCount:
@@ -31,40 +61,27 @@ class _HorasTrabalhadasCardState extends State<HorasTrabalhadasCard> {
               int entradaIndex = index * 2;
               int saidaIndex = entradaIndex + 1;
 
-              if (saidaIndex <
-                  widget.registroPontoSnapshot.registroPontoHojeList.length) {
-                String textoEntrada = widget.registroPontoSnapshot
-                            .registroPontoHojeList[entradaIndex].tipoRegistro ==
-                        "ENTRADA"
-                    ? widget.registroPontoSnapshot
-                        .registroPontoHojeList[entradaIndex].horaFormatada
-                    : "";
+              String textoEntrada = widget.registroPontoSnapshot
+                          .registroPontoHojeList[entradaIndex].tipoRegistro ==
+                      "ENTRADA"
+                  ? widget.registroPontoSnapshot
+                      .registroPontoHojeList[entradaIndex].horaFormatada
+                  : "";
 
-                String textoSaida = widget.registroPontoSnapshot
-                            .registroPontoHojeList[saidaIndex].tipoRegistro ==
-                        "SAIDA"
-                    ? widget.registroPontoSnapshot
-                        .registroPontoHojeList[saidaIndex].horaFormatada
-                    : "";
+              String textoSaida = saidaIndex <
+                          widget.registroPontoSnapshot.registroPontoHojeList
+                              .length &&
+                      widget.registroPontoSnapshot
+                              .registroPontoHojeList[saidaIndex].tipoRegistro ==
+                          "SAIDA"
+                  ? widget.registroPontoSnapshot
+                      .registroPontoHojeList[saidaIndex].horaFormatada
+                  : "";
 
-                return EntradasSaidasText(
-                  text1: textoEntrada,
-                  text2: textoSaida,
-                );
-              } else {
-                // Último par de ENTRADA sem SAIDA
-                String textoEntrada = widget.registroPontoSnapshot
-                            .registroPontoHojeList[entradaIndex].tipoRegistro ==
-                        "ENTRADA"
-                    ? widget.registroPontoSnapshot
-                        .registroPontoHojeList[entradaIndex].horaFormatada
-                    : "";
-
-                return EntradasSaidasText(
-                  text1: textoEntrada,
-                  text2: "",
-                );
-              }
+              return EntradasSaidasText(
+                text1: textoEntrada,
+                text2: textoSaida,
+              );
             },
           ),
         ],
@@ -74,14 +91,12 @@ class _HorasTrabalhadasCardState extends State<HorasTrabalhadasCard> {
 }
 
 class HorasTrabalhadas extends StatelessWidget {
-  const HorasTrabalhadas({
-    super.key,
-  });
+  const HorasTrabalhadas({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Text(
         '08:00 horas trabalhadas',
         style: TextStyle(
@@ -99,8 +114,11 @@ class EntradasSaidasText extends StatelessWidget {
   final String text1;
   final String text2;
 
-  const EntradasSaidasText(
-      {super.key, required this.text1, required this.text2});
+  const EntradasSaidasText({
+    Key? key,
+    required this.text1,
+    required this.text2,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -115,17 +133,19 @@ class EntradasSaidasText extends StatelessWidget {
             Text(
               text1,
               style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  fontFamily: AppLayoutDefaults.fontFamily),
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                fontFamily: AppLayoutDefaults.fontFamily,
+              ),
             ),
             const Spacer(flex: 5),
             Text(
               text2,
               style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  fontFamily: AppLayoutDefaults.fontFamily),
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                fontFamily: AppLayoutDefaults.fontFamily,
+              ),
             ),
             const Spacer(flex: 5),
           ],
