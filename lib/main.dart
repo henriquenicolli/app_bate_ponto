1fe32309394519c9700c96a08ee9f6ab68eba856
henriquenicolli <henrique.nicolli@gmail.com>
2024-05-02 21:14:34 -0300

    adicionado windows plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_bate_ponto/src/app.dart';
+import 'package:windows_plugin/windows_plugin.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  WindowsPlugin().getPlatformVersion().then((String? version) {
+    print(version);
+  });
+
   runApp(const AppBarApp());
 }
